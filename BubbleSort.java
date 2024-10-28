13aeb953f33046cbca00c1fd3f6c9308f5ee0e67
oshi2002 <oshani.kaveesha2002@gmail.com>
2024-10-28 22:16:31 +0530

    Bubble Sort Implementation/Ascending r & Dessending Order

---
+++ BubbleSort.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bubblesort;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class BubbleSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void assending(int[]array){
+         int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {  // Modify comparison for ascending order
+                    // Swap array[j] and array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void dessending(int[]array){
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] < array[j + 1]) {  // Modify comparison for descending order
+                    // Swap array[j] and array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[8];
+        
+        // Read 8 numbers from the user
+        System.out.println("Enter 8 numbers:");
+        for (int i = 0; i < 8; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+        
+        // Sort and display in ascending order
+        System.out.println("\nAscending order:");
+        assending(numbers);
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        // Sort and display in descending order
+        System.out.println("\n\nDescending order:");
+        dessending(numbers);
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        scanner.close();
+    }
+}
+    
+
